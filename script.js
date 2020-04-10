9d46bdfbf39e21b36c6f4794814466669cd078c8
dcashdallas <dcashdallas81@yahoo.com>
2020-04-10 17:09:16 -0500

    updating html, added char set

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+var randomCharString = "";
+var charSet = [
+    {
+        char: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~",
+        name: "Special Characters",
+        use: false
+    },
+    {
+        char: "0123456789",
+        name: "Number Characters",
+        use: false
+    },
+    {
+        char: "abcdefghijklmnopqrstuvwxyz",
+        name: "Lowercase Letters",
+        use: false
+    },
+    {
+        char: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        name: "Uppercase Letters",
+        use: false
+    }
+]
 
-// Write password to the #password input
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+var generate = () => {
+    var passwordLength = parseInt(prompt("Please select a password length between 8 and 128 characters."));
+    passwordString = "";
 
-    passwordText.value = password;
 
+    if (passwordLength > 8 && passwordLength < 129) {
+        charSet.forEach(set => {
+            const useChar = (prompt(`Do you want to use ${set.name}?`)).toLowerCase();
+            if (useChar === "yes" || useChar === "y") {
+                set.use = true;
+            }
+            if (set.use) {
+                randomCharString = randomCharString + set.char;
+            }
+        });
+        console.log(JSON.stringify(charSet))
+    }
+    else {
+        alert("Your password does not meet the necessary requirements.");
+    }
+
+    if (randomCharString !== "") {
+        for (i = 1; i <= passwordLength; i++) {
+            passwordString = passwordString + randomCharString.charAt(Math.floor(Math.random() * Math.floor((randomCharString.length) - 1)));
+        }
+
+        document.getElementById("password").value = passwordString;
+    }
+    else {
+        alert("Your password must be 8-128 characters in length & you must use at least one kind of character. Please select the Generate Password button and try again.");
+    }
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
